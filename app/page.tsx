89f17415f51d93e2eb467803364803bfdf9fa31e
maulshreegarg <mooney0406@gmail.com>
2024-09-09 05:49:45 +0530

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-violet-900 text-3xl p-4">THE Tina Times</h1>
+   
+    <div className="flex justify-between items-center float-none p-8">
+      <h1 className="text-violet-900 text-3xl font-bold font-serif float-left">THE Tina Times</h1>
+      <ul className="flex float-end p-8 font-serif">
+        <li>
+        <button type="button" className="p-2">Shop</button>
+        </li>
+        <li><button type="button" className="p-2">Blog</button>
+        </li>
+        <li> <button type="button" className="p-2">Who We Are</button>
+        </li>
+        <li>
+        <button type="button" className="p-2">My Profile</button>
+        </li>
+        <li>
+        <button type="button" className="bg-violet-800 text-white p-2 rounded-lg ">Basket</button>
+        
+        </li>
+      </ul>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        serif: ['Old Standard TT', 'serif'], 
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
